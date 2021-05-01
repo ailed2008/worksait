077c3b67092ca42bfa067ae3a9baf1dea6311d4d
JuanENP <juanjose_skorg@hotmail.com>
2021-05-01 09:03:41 -0500

    version 1

---
+++ assets/cart/index.php
@@ -0,0 +1,46 @@
+<?php
+echo "hola mundo";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- BOXICONS -->
+    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+    <!-- CSS -->
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+    <title>Ecommerce</title>
+</head>
+
+<body>
+    <!-- HEADER -->
+    <header class="l-header">
+        <nav class="nav bd-grid">
+            <div>
+                <a href="#" class="nav__logo">WORKSAIT</a>
+                <span> / <i class='bx bxl-paypal'></i>pago seguro</span>
+
+            </div>
+            <div>
+                <a href="../../index.html"><button type="button" class="boton">Seguir comprando</button></a>
+            </div>
+        </nav>
+    </header>
+
+    <main class="l-main">
+
+        <section>
+           <?php echo"<span>Sumario del artículo()</span>"?>
+        </section>
+
+    </main>
+
+    <!--===== MAIN JS =====-->
+    <script src="assets/js/main.js"></script>
+</body>
+
+</html>
